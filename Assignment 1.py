926b8243d3c9bc779ccaa00270dd9c1c0624549a
CodeGirlNancy <143062614+CodeGirlNancy@users.noreply.github.com>
2023-09-13 07:15:36 -0700

    Add files via upload

---
+++ Assignment 1.py
@@ -0,0 +1,44 @@
+#even_numbers
+even_numbers=(2, 4, 6)
+totaleven_numbers=2+4+6
+print("The sum of even numbers less than 8 is"+str(totaleven_numbers))
+print(totaleven_numbers)
+
+
+#The total number of people
+people=(3, 4, 5, 6, 7)
+total_people=3+4+5+6+7
+Average=(total_people)/5
+print(Average)
+
+
+-5+8*6
+C=-5
+d=8
+e=6
+f=d*e
+print(C+f)
+
+(55+9)% 9
+g=55
+h=9
+i=9
+print(g+h%i)
+
+
+20+-3*5/8
+k=20
+l=-3*(5/8)
+print(k+l)
+
+
+5+15/3*2-8%3
+m=5
+n=15
+p=3
+q=2
+r=-8
+s=3
+t=(5+(15/3)*2)-8
+print(t%s)
+        
